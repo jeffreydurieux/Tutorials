5e5428da18b9d536b6cc97b8964c5a45e07ae42f
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2019-07-28 19:27:10 -0700

    Update modRV.R
    test GitHub Desktop

--- modRV.R
+++ modRV.R
@@ -6,17 +6,16 @@
 
 # reference: Smilde AK, Kiers HAL, Bijlsma S, Rubingh CM, van Erk MJ (2009) Matrix correlations for high-dimen- sional data: the modified RV-coefficient. Bioinformatics 25(3):401–405
 
+# maybe not fully optimized for speed/memory?
 
 modRV <- function(X, Y){
-  
+
   XXtilde <- ( X %*% t(X) ) - diag (diag( X %*% t(X) ) )
   YYtilde <- ( Y %*% t(Y) ) - diag (diag( Y %*% t(Y) ) )
-  
-  res <-  ( t(c(XXtilde)) %*% c(YYtilde) ) / 
-    ( sqrt( ( t(c(XXtilde)) %*% c(XXtilde)) * ( t(c(YYtilde)) %*% c(YYtilde)) ) ) 
-  
-  
+
+  res <-  ( t(c(XXtilde)) %*% c(YYtilde) ) /
+    ( sqrt( ( t(c(XXtilde)) %*% c(XXtilde)) * ( t(c(YYtilde)) %*% c(YYtilde)) ) )
+
+
   return(res)
 }
-
-
